4a72d0fa7de8427ef527423a958e0ba9c5d967cd
ms5323 <33814617+ms5323@users.noreply.github.com>
2017-11-21 22:15:13 -0500

    Update CardGame.java

--- src/main/java/program/CardGame.java
+++ src/main/java/program/CardGame.java
@@ -5,9 +5,8 @@ import objects.Deck;
 public class CardGame {
     public static void main(String[] args) {
     		Deck deck = new Deck(); 
-    		System.out.println("Printing cards...");
+    		System.out.println("Generating Deck.....");
     		deck.generateDeck();
-    		System.out.println("Shuffling Cards...");
     		deck.shuffleDeck();
     		deck.showDeck();
     		int NumberOfDeals = 53;//number of times want a card to be dealt
